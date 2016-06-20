bf7243301a6b3950666f63e363de033f82241a42
Oliver Azevedo Barnes <oli.azevedo.barnes@gmail.com>
2016-06-20 16:37:33 -0300

    Phoenix project minus html and brunch

---
+++ lib/api/endpoint.ex
@@ -0,0 +1,37 @@
+defmodule Api.Endpoint do
+  use Phoenix.Endpoint, otp_app: :api
+
+  socket "/socket", Api.UserSocket
+
+  # Serve at "/" the static files from "priv/static" directory.
+  #
+  # You should set gzip to true if you are running phoenix.digest
+  # when deploying your static files in production.
+  plug Plug.Static,
+    at: "/", from: :api, gzip: false,
+    only: ~w(css fonts images js favicon.ico robots.txt)
+
+  # Code reloading can be explicitly enabled under the
+  # :code_reloader configuration of your endpoint.
+  if code_reloading? do
+    plug Phoenix.CodeReloader
+  end
+
+  plug Plug.RequestId
+  plug Plug.Logger
+
+  plug Plug.Parsers,
+    parsers: [:urlencoded, :multipart, :json],
+    pass: ["*/*"],
+    json_decoder: Poison
+
+  plug Plug.MethodOverride
+  plug Plug.Head
+
+  plug Plug.Session,
+    store: :cookie,
+    key: "_api_key",
+    signing_salt: "ZhK96t/9"
+
+  plug Api.Router
+end
