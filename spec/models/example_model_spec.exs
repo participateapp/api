031c90b8b498f56cf318f39154f47384f47ab549
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-07-08 20:39:31 -0300

    remove example specs that come with espec

--- spec/models/example_model_spec.exs
+++
@@ -1,6 +0,0 @@
-defmodule ParticipateApi.ExampleModelSpec do
-  use ESpec.Phoenix, model: ParticipateApi.Example
-
-  let :example, do: %ParticipateApi.Example{title: "Example 1"}
-  it do: example.title |> should(eq "Example 1")
-end
