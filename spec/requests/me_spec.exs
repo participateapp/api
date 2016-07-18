7fe10379f7a350f04ab45bfa7bdc8df63debec08
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-07-18 09:03:43 -0300

    WIP GET /me

--- spec/requests/me_spec.exs
+++ spec/requests/me_spec.exs
@@ -1,22 +1,32 @@
-defmodule ParticipateApi.TokensSpec do
+defmodule ParticipateApi.MeSpec do
   use ESpec.Phoenix, request: ParticipateApi.Endpoint
-  import ParticipateApi.Factory
+  # import ParticipateApi.Factory
+
+  alias ParticipateApi.Repo
+  alias ParticipateApi.Account
+  alias ParticipateApi.Participant
 
   describe "Me API" do
-    let :account, do: insert(:account)
-    let :current_participant, do: account.participant
-    let :token, do: Guardian.encode_and_sign(account)
-    let :headers do
-      %{
-        "Accept":        "application/vnd.api+json",
-        "Content-type":  "application/vnd.api+json",
-        "Authorization": "Bearer #{token}"
-      }
+    # can't get ex_machina to work yet
+    # let :account, do: insert(:account)
+    let :account do
+      participant = Repo.insert!(%Participant{name: "David Harvey"})
+      email_and_uid = %{email: "david@harvey.net", facebook_uid: "111111111"}      
+      build_account = Ecto.build_assoc(participant, :account, email_and_uid)
+      Repo.insert!(build_account)
     end
+    let :current_participant do
+      account.participant
+    end
+    let :token, do: Guardian.encode_and_sign(account)
 
     describe "GET /me" do
-
-      subject do: get "/me", %{}, headers
+      subject do
+        get(build_conn(), "/me")
+        |> put_req_header("accept", "application/vnd.api+json")
+        |> put_req_header("content-type", "application/vnd.api+json")
+        |> put_req_header("authorization", "Bearer #{token}")
+      end
 
       it "200 OK" do
         expect(subject).to have_http_status(200)
