586f7718e7c12eaa6f26fc8f21918808c7a4f145
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-07-22 08:22:05 -0300

    Account fields validation spec

--- spec/models/account_spec.exs
+++ spec/models/account_spec.exs
@@ -3,10 +3,22 @@ defmodule ParticipateApi.Models.AccountSpec do
   alias ParticipateApi.Account
 
   describe "#email" do
+    it "is required" do
+      changeset = Account.changeset(%Account{}, %{facebook_uid: "111"})
+      expect(changeset).to have_errors(email: {"can't be blank", []})
+    end
+
     it "is unique" do
       Repo.insert! %Account{email: "paulie@sopranos.tv", facebook_uid: "111"}
       changeset_for_dupe = Account.changeset(%Account{}, %{email: "paulie@sopranos.tv", facebook_uid: "222"})
       expect fn -> Repo.insert!(changeset_for_dupe) end  |> to(raise_exception)
+    end
+  end
+
+  describe "#facebook_uid" do
+    it "is required" do
+      changeset = Account.changeset(%Account{}, %{email: "paulie@sopranos.tv"})
+      expect(changeset).to have_errors(facebook_uid: {"can't be blank", []})
     end
   end
 end
