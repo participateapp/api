1f72feed5c12d02ffde65fbec7eb7d5344bc345a
Oliver Azevedo Barnes <oliverbwork@gmail.com>
2016-07-24 10:55:09 -0300

    Change migration adding participant_id to accounts, so accounts are deleted on participant deletion

--- priv/repo/migrations/20160711232831_account_belongs_to_participant.exs
+++ priv/repo/migrations/20160711232831_account_belongs_to_participant.exs
@@ -3,7 +3,7 @@ defmodule ParticipateApi.Repo.Migrations.AccountBelongsToParticipant do
 
   def change do
     alter table(:accounts) do
-      add :participant_id, :integer
+      add :participant_id, references(:participants, on_delete: :delete_all)
     end
   end
 end
